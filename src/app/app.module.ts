c7fc4a18ca3f72adb8abf0621a5e1091562f7c20
McKay1717 <nicolas1706ni@gmail.com>
2018-04-14 11:13:03 +0200

    Add Weather Icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import { WeatherIconsModule } from 'ngx-icons';
 
 import { AppComponent } from './app.component';
 import { MeteoComponent } from './meteo/meteo.component';
@@ -12,7 +15,12 @@ import { MeteoComponent } from './meteo/meteo.component';
     MeteoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WeatherIconsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
