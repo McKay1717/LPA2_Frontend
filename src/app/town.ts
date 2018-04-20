3a9dd84b60a9139897376f7111bc05088d35b699
McKay1717 <nicolas1706ni@gmail.com>
2018-04-21 00:00:44 +0200

    Fix null error

--- src/app/town.ts
+++ src/app/town.ts
@@ -1,11 +1,11 @@
 import { Weather } from './weather';
 
 export class Town {
-  id: number
-  town_name: string
-  latitude: number
-  longitude: number
-  weather: Weather
+  id: number = -1
+  town_name: string = " "
+  latitude: number = 0.0
+  longitude: number = 0.0
+  weather: Weather = null
 
     constructor(values: Object = {}) {
       Object.assign(this, values);
--- src/app/weather.ts
+++ src/app/weather.ts
@@ -1,11 +1,11 @@
 export class Weather {
-  summary: string
-  icon: string
-  precipIntensity: number
-  precipProbability: number
-  temperature: number
-  humidity: number
-  pressure: number
+  summary: string = " "
+  icon: string = " "
+  precipIntensity: number = 0.0
+  precipProbability: number = 0.0 
+  temperature: number = 0.0
+  humidity: number = 0.0
+  pressure: number = 0.0
 
     constructor(values: Object = {}) {
       Object.assign(this, values);
